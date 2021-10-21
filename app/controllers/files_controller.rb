a41d84a009e363d67db34ee93f642fa6af7b24c1
gaurav chauhan <rgbpremium@gmail.com>
2021-10-21 18:35:50 +1100

    first commit

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,19 +1,25 @@
 require 'search_in_file'
+require 'docx'
 class FilesController < ApplicationController
   def new
     if params[:path].to_s.length >0
       @path = Rails.root.to_s + '/' + params[:path].to_s
+      text = "#{params[:text].to_s}"
+
       @arrayFilesAll = Array.new # stores all files under path
       @contents = Array.new
       Dir.chdir(@path)
-      @arrayFilesAll = Dir.glob("**/*.{doc,docx,txt,pdf}",
+      @arrayFilesAll = Dir.glob("**/*.{doc,docx}",
                                 base: @path)
       # searchForContent
       @arrayFilesAll.each do |file|
-        hash_file = SearchInFile.search(file, '' )
-        if hash_file.size>0
+        hash_file = SearchInFile.search(file, text)
+        if hash_file.length!=0
           content = hash_file[0][:paragraphs].join('')
           @contents.append(content)
+
+
+
         end
       end
     end
